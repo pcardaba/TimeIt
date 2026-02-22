dd20ba143c9d9892131d166bb9868852aaca646b
Pablo Cardaba <pablo.cardaba@gmail.com>
2026-02-22 22:02:50 +0100

    Project main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+from .classes.timeitapp import TimeItApp
+
+
+def main():
+    root = tk.Tk()
+    app = TimeItApp(root)
+    ## Set widgeds style...
+
+    style = ttk.Style(root)
+    try:
+        style.theme_use("clam")
+    except tk.TclError:
+        pass
+    
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
